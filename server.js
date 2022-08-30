02b38b634cabc3e28a59e714a2b5fd609a6f9928
Ed <yide1986@gmail.com>
2022-08-31 00:38:27 +0800

    feat: 建立 server, 設置 cor, 設置回傳 JSON, 設定 options API

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const http = require('http');
 
 const requestListener = (req, res) => {
-  res.writeHeader(200, {"Content-Type":"text/plain"});
-  res.write("HelloWorld!");
-  res.end();
+  const headers = {
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json'
+  };
+
+  if (req.url == '/' && req.method == 'GET') {
+    res.writeHeader(200, headers);
+    res.write(JSON.stringify({
+      status: "success",
+      data: []
+    }));
+    res.end();
+  } else if (req.method == 'OPTIONS') {
+    res.writeHeader(200, headers);
+    res.end();
+  } else {
+    res.writeHeader(404, headers);
+    res.write(JSON.stringify({
+      status: "false",
+      message: "無此網站路由"
+    }));
+    res.end();
+  }
 }
 
 
