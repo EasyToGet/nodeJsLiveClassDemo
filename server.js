d0cd4ff8bacd300348f34e1b6ade135da90f84c7
Ed <yide1986@gmail.com>
2022-09-08 00:46:29 +0800

    feat: 新增 patch 編輯單筆代辦

--- server.js
+++ server.js
@@ -71,6 +71,27 @@ const requestListener = (req, res) => {
     } else {
       errorHandle(res);
     }
+  } else if (req.url.startsWith('/todos/') && req.method == 'PATCH') {
+    req.on('end', () => {
+      try {
+        const todo = JSON.parse(body).title;
+        const id = req.url.split('/').pop();
+        const index = todos.findIndex(element => element.id == id);
+        if (todo !== undefined && index !== -1) {
+          todos[index].title = todo;
+          res.writeHeader(200, headers);
+          res.write(JSON.stringify({
+            status: "success",
+            data: todos
+          }));
+          res.end();
+        } else {
+          errorHandle(res);
+        }
+      } catch (error) {
+        errorHandle(res);
+      }
+    });
   } else if (req.method == 'OPTIONS') {
     res.writeHeader(200, headers);
     res.end();
