af54aecf1149b187383aeb58d308053e63f69b6a
Ed <yide1986@gmail.com>
2022-09-24 17:19:21 +0800

    feat: 程式整個架構優化

---
+++ app.js
@@ -0,0 +1,9 @@
+const routes = require('./routes/index');
+
+require('./connections');
+
+const app = async (req, res) => {
+  routes(req, res);
+}
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const HttpControllers = require('../controllers/http');
+const PostsControllers = require('../controllers/posts');
+
+const routes = async (req, res) => {
+  const { url, method } = req;
+  let body = "";
+
+  req.on('data', chunk => {
+    body += chunk;
+  });
+
+  if (url == '/posts' && method == 'GET') {
+    PostsControllers.getPosts(req, res);
+  } else if (url == '/posts' && method == 'POST') {
+    req.on('end', async () => {
+      PostsControllers.createdPosts({ body, req, res });
+    })
+  } else if (url == '/posts' && method == 'DELETE') {
+    PostsControllers.deleteAll(req, res);
+  } else if (url.startsWith('/posts/') && method == 'DELETE') {
+    PostsControllers.deleteSingle(req, res);
+  } else if (url.startsWith('/posts/') && method == 'PATCH') {
+    req.on('end', async () => {
+      PostsControllers.patchPosts({ body, req, res });
+    });
+  } else if (method == 'OPTIONS') {
+    HttpControllers.cors(req, res);
+  } else {
+    HttpControllers.notFound(req, res);
+  }
+};
+
+module.exports = routes;
